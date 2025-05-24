10f2b75c55d4247ab5bb78f10aae37f5b48e8f0d
unknown <diloch111@gmail.com>
2025-05-24 02:40:51 +0100

    add styling to nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div className="mt-12 px-36">
       <Router>
         <Navbar />
       </Router>
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,19 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-end  m-4">
-      <Link className="flex justify-center items-center w-24 h-24 p-2 bg-gray rounded-full" to="/">
-        <img className="" src="/bta-logo-light.png" alt="project bta logo" />
+    <nav className="flex justify-between items-center">
+      <Link
+        className="flex justify-center items-center"
+        to="/"
+      >
+        <img className="w-48" src="/bta-logo-light.png" alt="project bta logo" />
       </Link>
-      <div className="flex font-abel">
-        <Link to="/">Home</Link>        
-        <Link to="/mission">Mission</Link>
-        <Link to="/campaigns">Campaigns</Link>
-        <Link to="/Donate">Donate</Link>
-        <button>Join</button>
+      <div className="flex font-abel gap-6 justify-center items-center">
+        <Link className="hover:font-bold  transition" to="/">Home</Link>
+        <Link className="hover:font-bold transition" to="/mission">Mission</Link>
+        <Link className="hover:font-bold transition" to="/campaigns">Campaigns</Link>
+        <Link className="hover:font-bold transition" to="/Donate">Donate</Link>
+        <button className="border font-bold text-secondary transition hover:rounded-lg px-4 py-2">Join</button>
       </div>
     </nav>
   );
