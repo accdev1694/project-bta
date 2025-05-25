74549767e99d6ca4d474a014526ba0fa217c7889
unknown <diloch111@gmail.com>
2025-05-25 02:43:39 +0100

    set up partners page

--- src/components/sections/Partners.jsx
+++ src/components/sections/Partners.jsx
@@ -1,16 +1,72 @@
 import HeadingOne from "../HeadingOne";
 import HeadingTwo from "../HeadingTwo";
+import { partners } from "../../utils.js";
 
 const Partners = () => {
-    
+  if (!partners || partners.length < 1) {
+    return;
+  }
 
-    return ( <section id="partners" className="min-h-screen pt-21 text-center">
-        <HeadingOne title="Meet the passionate individuals behind our mission." className="mb-6"/>
-        <HeadingTwo title="Our Partners" className="text-primary" />
-        <div className="grid grid-cols-4">
+  return (
+    <section id="partners" className="min-h-screen pt-21 text-center bg-white">
+      <HeadingOne
+        title="Meet the passionate individuals behind our mission."
+        className="mb-6"
+      />
+      <HeadingTwo title="Our Partners" className="text-primary" />
+      <div className="grid grid-cols-6 mt-4">
+        {partners.map((partner) => (
+          <div
+            key={partner.id}
+            className="flex flex-col items-center justify-between mt-12"
+          >
+            <img
+              className="rounded-full w-18"
+              src={partner.image}
+              alt={partner.name}
+            />
+            <h3 className="mt-2 font-bold text-sm font-abel tracking-wide">
+              {partner.name}
+            </h3>
+            <p className="text-sm font-inria">{partner.position}</p>
+            <p className="text-xs font-inria w-48 mt-4">
+              {partner.description}
+            </p>
+            <div className="flex justify-center items-center gap-4 mt-4">
+              <a href={partner.handles.instagram}>
+                <img
+                  className="w-4"
+                  src="/images/instagram.png"
+                  alt="Instagram logo"
+                />
+              </a>
+              <a href={partner.handles.facebook}>
+                <img
+                  className="w-4"
+                  src="/images/facebook.png"
+                  alt="Facebook logo"
+                />
+              </a>
+              <a href={partner.handles.twitter}>
+                <img
+                  className="w-4"
+                  src="/images/twitter.png"
+                  alt="Twitter logo"
+                />
+              </a>
+              <a href={partner.handles.linkedin}>
+                <img
+                  className="w-4"
+                  src="/images/linkedin.png"
+                  alt="LinkedIn logo"
+                />
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-        </div>
-    </section> );
-}
- 
 export default Partners;
---
+++ src/utils.js
@@ -0,0 +1,88 @@
+export const partners = [
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFv8yKLUE-rGAPYj41Bigfne3zUWW-TD0Z7A&s",
+    name: "Comfort Aboje",
+    position: "Founder/CEO",
+    description:
+      "Comfort Aboje is the founder of Comfort Aboje Foundation, ...",
+    handles: {
+      instagram: "https://www.instagram.com/comfortaboje/",
+      facebook: "https://www.facebook.com/comfortaboje",
+      twitter: "https://twitter.com/comfortaboje",
+      linkedin: "https://www.linkedin.com/in/comfortaboje/",
+    },
+    key: 1,
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFv8yKLUE-rGAPYj41Bigfne3zUWW-TD0Z7A&s",
+    name: "Oloche Aboje",
+    position: "Co-Founder",
+    description:
+      "Oloche Aboje is the co-founder of Comfort Aboje Foundation, ...",
+    handles: {
+      instagram: "https://www.instagram.com/partner2/",
+      facebook: "https://www.facebook.com/partner2",
+      twitter: "https://twitter.com/partner2",
+      linkedin: "https://www.linkedin.com/in/partner2/",
+    },
+    key: 2,
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFv8yKLUE-rGAPYj41Bigfne3zUWW-TD0Z7A&s",
+    name: "Terdoo Oladipupo",
+    position: "Regional Director",
+    description: "Terdoo Oladipupo is the Regional Director ...",
+    handles: {
+      instagram: "https://www.instagram.com/partner3/",
+      facebook: "https://www.facebook.com/partner3",
+      twitter: "https://twitter.com/partner3",
+      linkedin: "https://www.linkedin.com/in/partner3/",
+    },
+    key: 3,
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFv8yKLUE-rGAPYj41Bigfne3zUWW-TD0Z7A&s",
+    name: "Solomon Gana",
+    position: "Position Four",
+    description: "Solomon Gana is a partner with Comfort Aboje Foundation",
+    handles: {
+      instagram: "https://www.instagram.com/partner4/",
+      facebook: "https://www.facebook.com/partner4",
+      twitter: "https://twitter.com/partner4",
+      linkedin: "https://www.linkedin.com/in/partner4/",
+    },
+    key: 4,
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFv8yKLUE-rGAPYj41Bigfne3zUWW-TD0Z7A&s",
+    name: "Partner Five",
+    position: "Position Five",
+    description: "Partner Five is a partner with Comfort Aboje Foundation",
+    handles: {
+      instagram: "https://www.instagram.com/partner5/",
+      facebook: "https://www.facebook.com/partner5",
+      twitter: "https://twitter.com/partner5",
+      linkedin: "https://www.linkedin.com/in/partner5/",
+    },
+    key: 5,
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFv8yKLUE-rGAPYj41Bigfne3zUWW-TD0Z7A&s",
+    name: "Partner Six",
+    position: "Position Six",
+    description: "Partner Six is a partner with Comfort Aboje Foundation",
+    handles: {
+      instagram: "https://www.instagram.com/partner6/",
+      facebook: "https://www.facebook.com/partner6",
+      twitter: "https://twitter.com/partner6",
+      linkedin: "https://www.linkedin.com/in/partner6/",
+    },
+    key: 6,
+  },
+];
